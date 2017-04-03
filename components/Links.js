f5d2e9cf0ad7c42a2a249098cb8d2feb8268e41f
gness1804 <graham.nessler@gmail.com>
2017-04-03 17:09:47 -0600

    Finish adding links

--- components/Links.js
+++ components/Links.js
@@ -30,6 +30,84 @@ class Links extends Component {
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => {
+            Linking.openURL('https://medium.com/javascript-scene/10-interview-questions-every-javascript-developer-should-know-6fa6bdf5ad95')
+          }}
+        >
+          <Text
+            style={styles.text}
+          >
+          Ten interview questions every JavaScript developer should know
+        </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL('https://www.interviewcake.com/')
+          }}
+        >
+          <Text
+            style={styles.text}
+          >
+          Interview Cake
+        </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL('http://javascriptissexy.com/16-javascript-concepts-you-must-know-well/')
+          }}
+        >
+          <Text
+            style={styles.text}
+          >
+          JavaScript is Sexy: 16 Core Concepts for JS Professionals
+        </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL('https://addyosmani.com/resources/essentialjsdesignpatterns/book/')
+          }}
+        >
+          <Text
+            style={styles.text}
+          >
+          Learning JavaScript Design Patterns (Full Book Online)
+        </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL('http://www.skilledup.com/articles/25-css-interview-questions-answers')
+          }}
+        >
+          <Text
+            style={styles.text}
+          >
+          25 Helpful CSS Interview Questions and Answers
+        </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL('https://tylermcginnis.com/react-interview-questions/')
+          }}
+        >
+          <Text
+            style={styles.text}
+          >
+          React Interview Questions
+        </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL('https://blog.risingstack.com/node-js-interview-questions-and-answers-2017/')
+          }}
+        >
+          <Text
+            style={styles.text}
+          >
+          Node.js Interview Questions and Answers (2017 Edition)
+        </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => {
             navigate('Home')
           }}
         >
